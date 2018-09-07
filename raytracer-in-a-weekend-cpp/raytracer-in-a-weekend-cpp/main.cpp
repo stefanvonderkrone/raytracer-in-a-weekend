07902d54a98941e6ba6ddb1d250aed713508c022
Stefan von der Krone <stefanvonderkrone@gmx.de>
2018-09-07 22:41:12 +0200

    front: initial xcode project.

---
+++ raytracer-in-a-weekend-cpp/raytracer-in-a-weekend-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  raytracer-in-a-weekend-cpp
+//
+//  Created by Stefan von der Krone on 07.09.18.
+//  Copyright © 2018 Stefan von der Krone. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
