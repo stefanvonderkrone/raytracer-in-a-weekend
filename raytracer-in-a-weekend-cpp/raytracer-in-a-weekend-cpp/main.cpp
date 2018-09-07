6feec00166de9bf618a9ee466551ef7f186e47db
Stefan von der Krone <stefanvonderkrone@gmx.de>
2018-09-07 23:07:28 +0200

    chapter 2.

--- raytracer-in-a-weekend-cpp/raytracer-in-a-weekend-cpp/main.cpp
+++ raytracer-in-a-weekend-cpp/raytracer-in-a-weekend-cpp/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include "vec3.h"
 
 int main() {
     int nx = 200;
@@ -14,12 +15,10 @@ int main() {
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99 * col.r());
+            int ig = int(255.99 * col.g());
+            int ib = int(255.99 * col.b());
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
