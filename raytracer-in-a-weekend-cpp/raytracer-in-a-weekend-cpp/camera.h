ad39bdb20281dd656d15f2d130819a2257f4c81b
Stefan von der Krone <stefanvonderkrone@gmx.de>
2018-09-08 12:27:57 +0200

    chapter 6

---
+++ raytracer-in-a-weekend-cpp/raytracer-in-a-weekend-cpp/camera.h
@@ -0,0 +1,34 @@
+//
+//  camera.h
+//  raytracer-in-a-weekend-cpp
+//
+//  Created by Stefan von der Krone on 08.09.18.
+//  Copyright © 2018 Stefan von der Krone. All rights reserved.
+//
+
+#ifndef camera_h
+#define camera_h
+
+#include "ray.h"
+
+class camera {
+public:
+    camera() {
+        lower_left_corner = vec3(-2.0, -1.0, -1.0);
+        horizontal = vec3(4.0, 0.0, 0.0);
+        vertical = vec3(0.0, 2.0, 0.0);
+        origin = vec3(0.0, 0.0, 0.0);
+    }
+    
+    ray get_ray(float u, float v) {
+        return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+    };
+    
+    vec3 origin;
+    vec3 lower_left_corner;
+    vec3 horizontal;
+    vec3 vertical;
+};
+
+
+#endif /* camera_h */
